8e3a0d84bac435c4c4ae347b1c0874fc9b75bea8
JuanCor253 <juan.cortes-gu@mail.escuelaing.edu.co>
2025-03-10 22:18:16 -0500

    Ejercicios de clasificación y suma de polinomios

--- clase/eje_5/clasificar.cpp
+++ clase/eje_5/clasificar.cpp
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 
-int main(int argc, char **argv){
-
-    cout << "He recibido " << argc << " argumentos de los cuales" << endl;
-    for (int i = 0; i < argc; i++)
-    {
-        cout << "Argumento " << i << " es " << argv[i] << endl;
+int main(int argc, char** argv){
+  cout << "He recibido " << argc << " argumentos de los cuales: " << endl;
+  for(int i = 0; i < argc; i++){
+    for(int j = 0; j < strlen(argv[i]); j++){
+      if(!isdigit(argv[i][j]) && argv[i][j] != '.'){
+        cout << "El argumento '" << argv[i] << "' no es un número" << endl;
+        cout << "    - tiene " << strlen(argv[i]) << " caracteres" << endl;
+        break;  
+      }
     }
+  }
+  return 0;
 }
+
